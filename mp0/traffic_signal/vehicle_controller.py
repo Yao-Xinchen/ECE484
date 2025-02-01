cc06f3708811ed5238a173f0dd3bf7e214a9e7f1
Yao-Xinchen <yao29@illinois.edu>
2025-02-01 15:33:58 -0600

    complete mp9 p2 controller

--- mp0/traffic_signal/vehicle_controller.py
+++ mp0/traffic_signal/vehicle_controller.py
@@ -22,7 +22,6 @@ class State:
     theta: float
     v: float
     agent_mode: VehicleMode
-    signal_mode: TLMode
 
     def __init__(self, x, y, theta, v, agent_mode: VehicleMode):
         pass
@@ -33,13 +32,51 @@ def decisionLogic(ego: State, other: State):
 
     # TODO: Edit this part of decision logic
 
-    if ego.agent_mode == VehicleMode.Normal and other.signal_mode == TLMode.RED:
-        output.agent_mode = VehicleMode.HardBrake
+    d_in = 20
+    d_out = 15
+
+    brake_dist = 18.25  # distance needed to brake
+
+    if ego.agent_mode == VehicleMode.Normal:
+        if other.signal_mode == TLMode.RED:
+            if ego.x > other.x - d_in:
+                # exit
+                output.agent_mode = VehicleMode.Accel
+            if other.x - d_out - brake_dist < ego.x < other.x - d_out:
+                # too late for exit
+                output.agent_mode = VehicleMode.HardBrake
+        if other.signal_mode == TLMode.GREEN:
+            output.agent_mode = VehicleMode.Accel
+        if other.signal_mode == TLMode.YELLOW:
+            if ego.x > other.x - d_in:
+                # exit
+                output.agent_mode = VehicleMode.Accel
+            if other.x - d_in - brake_dist < ego.x < other.x - d_in:
+                # too late for exit
+                output.agent_mode = VehicleMode.HardBrake
+
+    if ego.agent_mode == VehicleMode.Accel:
+        if other.signal_mode == TLMode.RED:
+            if other.x - d_out - brake_dist < ego.x < other.x - d_out:
+                # too late for exit
+                output.agent_mode = VehicleMode.HardBrake
+        if other.signal_mode == TLMode.YELLOW:
+            if other.x - d_in - brake_dist < ego.x < other.x - d_in:
+                # too late for exit
+                output.agent_mode = VehicleMode.HardBrake
+
+    if ego.agent_mode == VehicleMode.HardBrake:
+        if other.signal_mode == TLMode.GREEN:
+            # resume speed
+            output.agent_mode = VehicleMode.Accel
 
     ###########################################
 
-    assert not (other.signal_mode == TLMode.RED and (ego.x > other.x - 20 and ego.x < other.x - 15)), "Run Red Light"
-    assert not (other.signal_mode == TLMode.RED and (
-                ego.x > other.x - 15 and ego.x < other.x) and ego.v < 1), "Stop at Intersection"
+    assert not (other.signal_mode == TLMode.RED
+                and (ego.x > other.x - 20 and ego.x < other.x - 15)
+                ), "Run Red Light"
+    assert not (other.signal_mode == TLMode.RED
+                and (ego.x > other.x - 15 and ego.x < other.x) and ego.v < 1
+                ), "Stop at Intersection"
 
     return output
