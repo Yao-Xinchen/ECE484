36bdd3c03cbfc72a529b99ed541ce6fe12f399e6
Yao-Xinchen <yao29@illinois.edu>
2025-03-28 18:09:28 -0500

    enable plotting

--- mp3/src/mp3/src/particle_filter.py
+++ mp3/src/mp3/src/particle_filter.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from maze import Maze, Particle, Robot
 import bisect
 import rospy
@@ -79,7 +80,7 @@ class particleFilter:
             rospy.loginfo("Service did not process request: "+str(exc))
         return modelState
 
-    def weight_gaussian_kernel(self,x1, x2, std = 5000):
+    def weight_gaussian_kernel(self,x1, x2, std = 1):
         if x1 is None: # If the robot recieved no sensor measurement, the weights are in uniform distribution.
             return 1./len(self.particles)
         else:
@@ -99,14 +100,17 @@ class particleFilter:
         ## TODO #####
         weights = []
         for p in self.particles:
-            measurements_particle = p.senseWalls()  
-            w = self.weight_gaussian_kernel(readings_robot, measurements_particle, std=5000)
+            measurements_particle = p.read_sensor() 
+            # Assign weights using Gaussian
+            w = self.weight_gaussian_kernel(readings_robot, measurements_particle, std=50)
             weights.append(w)
 
         total_weight = sum(weights)
         if total_weight > 0:
+            # Normalize weights to sum to 1
             weights = [w / total_weight for w in weights]
         else:
+            # If all weights are zero, assign uniform weights
             weights = [1.0 / len(self.particles)] * len(self.particles)
 
         for i, p in enumerate(self.particles):
@@ -125,19 +129,21 @@ class particleFilter:
         ## TODO #####
         
         weights = [p.weight for p in self.particles]
+        weights = np.sort(weights)
         cumulative_sum = np.cumsum(weights)
 
         for i in range(self.num_particles):
-            rand_val = random.random()
+            rand_val = random.uniform(0, cumulative_sum[-1])
             idx = bisect.bisect_left(cumulative_sum, rand_val)
             chosen_particle = self.particles[idx]
             new_particle = Particle(
-                x=chosen_particle.x,
-                y=chosen_particle.y,
-                maze=self.world,
-                sensor_limit=self.sensor_limit
+            x=chosen_particle.x,
+            y=chosen_particle.y,
+            heading=chosen_particle.heading,
+            maze=self.world,
+            sensor_limit=self.sensor_limit,
+            noisy=True
             )
-            new_particle.theta = chosen_particle.theta
             particles_new.append(new_particle)
 
         ###############
@@ -154,17 +160,15 @@ class particleFilter:
         
         if not self.control:
             return
-
-        vr, delta = self.control[-1]
-
-        dt = 0.1
-        for p in self.particles:
-            dx = vr * np.cos(p.theta)
-            dy = vr * np.sin(p.theta)
-            dtheta = delta
-            p.x += dx * dt
-            p.y += dy * dt
-            p.theta += dtheta * dt
+        dt = 0.01
+        for (vr, delta) in self.control:
+            for p in self.particles:
+                dx = vr * np.cos(p.heading)
+                dy = vr * np.sin(p.heading)
+                dheading = delta
+                p.x += dx * dt
+                p.y += dy * dt
+                p.heading += dheading * dt
 
         ###############
         # pass
@@ -176,23 +180,52 @@ class particleFilter:
             Run PF localization
         """
         count = 0 
+        timestep = []
+        error_pos = []
+        error_head = []
+        fig, ax = plt.subplots(2,1)
+        ax1 = ax[0]
+        ax2 = ax[1]
+        pos_err_line, = ax1.plot([], [], 'b',)
+        head_err_line, = ax2.plot([], [], 'r')
+        ax1.set_title("Position Error")
+        ax2.set_title("Heading Error")
+        fig.show()
         while True:
             ## TODO: (i) Implement Section 3.2.2. (ii) Display robot and particles on map. (iii) Compute and save position/heading error to plot. #####
-
+            count = count + 1
+            timestep.append(count)
             # 1
             self.particleMotionModel()
             reading = self.bob.read_sensor()
             self.updateWeight(reading)
             self.resampleParticle()
+            print(f"Particle count: {len(self.particles)}")
 
             # 2
             self.world.show_maze()
+            self.world.clear_objects()
             self.world.show_particles(self.particles)
             self.world.show_robot(self.bob)
 
             # 3
             x_error = self.bob.x - self.particles[0].x
             y_error = self.bob.y - self.particles[0].y
-            theta_error = self.bob.theta - self.particles[0].theta
-            
+            heading_error = self.bob.heading - self.particles[0].heading
+            error_pos.append(np.linalg.norm([x_error, y_error]))
+            error_head.append(heading_error)
+
+            pos_err_line.set_xdata(timestep)
+            pos_err_line.set_ydata(error_pos)
+            head_err_line.set_xdata(timestep)
+            head_err_line.set_ydata(error_head)
+
+            ax1.relim()
+            ax2.relim()
+            ax1.autoscale_view()
+            ax2.autoscale_view()
+
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+
             ###############
